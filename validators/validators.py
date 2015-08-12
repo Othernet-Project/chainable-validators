5040ad951966b73e937d4e3909189d6025f48ce0
Ben Ennis <ben@outernet.is>
2015-08-12 07:29:35 -0700

    moved content_type to outernet_metadata

--- validators/validators.py
+++ validators/validators.py
@@ -12,7 +12,6 @@ import re
 import datetime
 
 from .re_patterns import URL_RE
-from .helpers import spec_validator
 from .chain import chainable, ReturnEarly
 
 RELPATH_RE = re.compile(r'^[^/]+(/[^/]+)*$')
@@ -150,65 +149,5 @@ def min_len(min=1):
         if  v == None or len(v) < min:
             raise ValueError("Key must be longer than {}, was {}".format(min,v),
                              'min_length')
-        return v
-    return validator
-
-
-def content_type(TYPE_SPECS):
-    @chainable
-    def validator(v):
-        errors = {}
-        for key in v:
-            value = v[key]
-            key_string = 'content.{}'.format(key)
-
-            if key not in TYPE_SPECS:
-                errors['content'] = {
-                    key: ValueError('content type must be one of '
-                                   '{}'.format(CONTENT_TYPES), 'content_type')}
-            elif type(value) != dict:
-                errors['content'] = {
-                    key: ValueError('{} must be a '
-                                    'dict'.format(key), 'content_type')}
-            else:
-                VALIDATOR = spec_validator(
-                    TYPE_SPECS[key], key=lambda k: lambda obj: obj.get(k))
-                e = VALIDATOR(value)
-                if e:
-                    errors[key_string] = e
-                elif key == 'audio':
-                    i = 0
-                    spec = TYPE_SPECS['audio.playlist']
-                    VALIDATOR = spec_validator(
-                        spec, key=lambda k: lambda obj: obj.get(k))
-                    for item in value['playlist']:
-                        i += 1
-                        e = VALIDATOR(item)
-                        if e:
-                            s = key_string + '.' + str(i)
-                            errors[s] = e
-                elif key == 'image':
-                    i = 0
-                    spec = TYPE_SPECS['image.album']
-                    VALIDATOR = spec_validator(
-                        spec, key=lambda k: lambda obj: obj.get(k))
-                    for item in value['album']:
-                        i += 1
-                        e = VALIDATOR(item)
-                        if e:
-                            s = key_string + '.' + str(i)
-                            errors[s] = e
-        final_set = []
-        if errors:
-            for key in errors:
-                set = []
-                for k in errors[key]:
-                    key_string = '.'.join([key, k])
-                    msg = errors[key][k].args[0]
-                    string = '{}: {}'.format(key_string, msg)
-                    set.append(string)
-                final_set.append('\n'.join(set))
-            error_string = '\n' + '\n'.join(final_set)
-            raise ValueError(error_string, 'content_type')
         return v
     return validator
